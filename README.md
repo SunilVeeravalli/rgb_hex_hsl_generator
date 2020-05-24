ab0706e82e50fd8aaa60a02c16ef9fe29bf8b126
Sunil Veeravalli <veeravallisunil@gmail.com>
2020-05-24 19:00:49 +0100

    Updated README.md and rgb_hex_hsl_generator.py

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# rgb_hex_hsl_generator
-Generates distinct RGB, HEX and HSL color code format
+# RGB, HEX and HSL color codes generator
+
+## Introduction
+
+### RGB
+For computers, television, mobile devices etc., RGB (Red Green Blue) is the most commonly used color profile. Other colors are generated from the combinations of red, green and blue colors.  
+
+Each color in RGB can be represented using a 0 to 255 scale or a 0.0 to 1.0 scale.
+
+For example: RGB values for the color Magenta is (202, 31, 123) on a 0 to 255 scale or (0.79215, 0.12156, 0.48235) on a 0.0 to 1.0 scale
+
+### HEX
+Hexa decimal representation of colors is mostly sen in web designers and developers. HEX color is a six characters alpha numeric generated from RGB values.
+
+For example: the HEX code for the color Magenta is #CA1F7B
+
+### HSL
+HSL (Hue, Saturation, Luminance or Lightness) is also an alternative representation of RGB values. The value of hue is in degrees where as the values for saturation and Luminance are in percentages.
+
+For example: the HSL code for the color Magenta is (328, 73, 46) where 328<sup>o</sup> is Hue, 73% is Saturation and 46% is Luminance.
+
+
+## User guide
+I developed a program that generates distinct and random color values in RGB, HEX and HSL format. Let me show you the usage in an IDE vs Terminal with some examples.
+
+Note:  
+- The RGB values on a scale 0 to 255 will be created first and these values will be used as a source to generate RGB values on a scale 0.0 to 1.0 as well as for HEX and HSL calculations.
+
+### 1. Using an IDE or Jupyter notebook:
+Run the script `rgb_hex_hsl_generator.py` which will install the required libraries mentioned in `requirements.txt` and imports them and creates the class object **ColorCodesGenerator** in memory.
+
+To generate five colors:
+```python
+five_colors = ColorCodesGenerator(n_colors = 5)
+```
+If you are interested in RGB values on a scale 0 to 255, then:
+```python
+five_colors.rgb_0_255()
+[(166, 98, 106), (14, 127, 8), (228, 13, 42), (247, 158, 235), (35, 77, 115)]
+```
+If you are interested in RGB values on a scale 0.0 to 1.0, then:
+```python
+five_colors.rgb_0_1()
+[(0.65098, 0.38431, 0.41569), (0.0549, 0.49804, 0.03137), (0.89412, 0.05098, 0.16471), (0.96863, 0.61961, 0.92157), (0.13725, 0.30196, 0.45098)]
+```
+If you are interested in HEX values, then:
+```python
+five_colors.hex_codes()
+['#A6626A', '#0E7F08', '#E40D2A', '#F79EEB', '#234D73']
+```
+If you are interested in HSL values, then:
+```python
+five_colors.hsl_codes()
+[(353, 28, 52), (117, 88, 26), (352, 89, 47), (308, 85, 79), (208, 53, 29)]
+```
+To save all the color codes as a data frame, then:
+```python
+five_colors_df = five_colors.all_in_dataframe()
+print(five_colors_df)
+
+         rgb_0_255                      rgb_0_1 hex_codes      hsl_codes
+0   (166, 98, 106)  (0.65098, 0.38431, 0.41569)   #A6626A  (353, 28, 52)
+1     (14, 127, 8)   (0.0549, 0.49804, 0.03137)   #0E7F08  (117, 88, 26)
+2    (228, 13, 42)  (0.89412, 0.05098, 0.16471)   #E40D2A  (352, 89, 47)
+3  (247, 158, 235)  (0.96863, 0.61961, 0.92157)   #F79EEB  (308, 85, 79)
+4    (35, 77, 115)  (0.13725, 0.30196, 0.45098)   #234D73  (208, 53, 29)
+```
+
+There is not need to go in a sequence like: first rgb_0_255(), then rgb_0_1(), then hex_codes() etc. One can directly request say hex_codes() after initializing the class as shown below.
+```python
+ten_colors = ColorCodesGenerator(n_colors = 10)
+ten_colors.hex_codes()
+['#84D691', '#A8E07F', '#3037C4', '#D5FB43', '#341000', '#83702D', '#C4385E', '#EAEA5A', '#A1D547', '#475569']
+```
+Or, one can directly generate the data frame by:
+```python
+twenty_colors = ColorCodesGenerator(n_colors = 20)
+twenty_colors.all_in_dataframe()
+
+          rgb_0_255                      rgb_0_1 hex_codes      hsl_codes
+0   (221, 180, 109)  (0.86667, 0.70588, 0.42745)   #DDB46D   (38, 62, 65)
+1    (112, 159, 40)  (0.43922, 0.62353, 0.15686)   #709F28   (84, 60, 39)
+2     (47, 97, 175)  (0.18431, 0.38039, 0.68627)   #2F61AF  (217, 58, 44)
+3     (34, 186, 66)  (0.13333, 0.72941, 0.25882)   #22BA42  (133, 69, 43)
+4     (141, 41, 52)  (0.55294, 0.16078, 0.20392)   #8D2934  (353, 55, 36)
+5      (209, 12, 6)  (0.81961, 0.04706, 0.02353)   #D10C06    (2, 94, 42)
+6      (39, 43, 47)  (0.15294, 0.16863, 0.18431)   #272B2F   (210, 9, 17)
+7      (50, 62, 55)  (0.19608, 0.24314, 0.21569)   #323E37  (145, 11, 22)
+8   (224, 195, 147)  (0.87843, 0.76471, 0.57647)   #E0C393   (37, 55, 73)
+9     (20, 241, 11)   (0.07843, 0.9451, 0.04314)   #14F10B  (118, 91, 49)
+10  (162, 106, 229)  (0.63529, 0.41569, 0.89804)   #A26AE5  (267, 70, 66)
+11   (249, 119, 87)  (0.97647, 0.46667, 0.34118)   #F97757   (12, 93, 66)
+12   (27, 148, 220)  (0.10588, 0.58039, 0.86275)   #1B94DC  (202, 78, 48)
+13  (142, 139, 111)   (0.55686, 0.5451, 0.43529)   #8E8B6F   (54, 12, 50)
+14  (178, 245, 139)   (0.69804, 0.96078, 0.5451)   #B2F58B   (98, 84, 75)
+15   (240, 24, 146)  (0.94118, 0.09412, 0.57255)   #F01892  (326, 88, 52)
+16    (40, 15, 249)  (0.15686, 0.05882, 0.97647)   #280FF9  (246, 95, 52)
+17    (74, 46, 192)   (0.2902, 0.18039, 0.75294)   #4A2EC0  (252, 61, 47)
+18    (75, 190, 88)    (0.29412, 0.7451, 0.3451)   #4BBE58  (127, 47, 52)
+19  (118, 146, 125)   (0.46275, 0.57255, 0.4902)   #76927D  (135, 11, 52)
+```
+
+### 2. Using a CLI terminal:
+
+
+
--- rgb_hex_hsl_generator.py
+++ rgb_hex_hsl_generator.py
@@ -1,16 +1,22 @@
 import os
-os.sys("pip install -r requirements.txt")
-
+os.system("python -m pip install -r requirements.txt")
 import numpy as np
-
-# Creating a function that will create random rgb colors
-
-
+import pandas as pd
 
 
 class ColorCodesGenerator:
+    """
+    Generates rgb, hex and hsl color codes
+    """
     
     def __init__(self, n_colors):
+        """
+        n_colors: an integer
+            The value should be <= 16777216 since the number of possible rgb combinations are 256 * 256 * 256 = 16777216
+        """
+        assert isinstance(n_colors, int), 'n_colors takes only integer values'
+        assert n_colors <= (256 * 256 * 256), 'n_colors should be an integer value less than or equal to 16777216'
+        
         self.n_colors = n_colors
         self.__rgb_0_255_list = None
         self.__rgb_0_1_list = []
@@ -19,7 +25,7 @@ class ColorCodesGenerator:
     
     @staticmethod
     def suffix_0s(value: str = None) -> str:
-        '''
+        """
         Suffixes a single character hexa decimal value with 0.
         
         Parameter
@@ -33,15 +39,22 @@ class ColorCodesGenerator:
         Example:
             if value = '0', the output will be '00'
             if value = 'a', the output will be '0A'
-        '''
+        """
         
         if len(value) < 2:
             return '0' + value
         else:
             return value
-        
+    
+    @staticmethod
+    def random_int_generator() -> int:
+        """
+        Returns one random integer between 0 and 255
+        """
+        return np.random.randint(low = 0, high = 255, size = 1).tolist().pop()
+    
     def rgb_0_255(self) -> list:
-        '''
+        """
         Generates rgb colors in range 0 to 255.
         
         Returns
@@ -50,37 +63,34 @@ class ColorCodesGenerator:
         Example:
             If n_colors = 2, the output will be:
             [(23, 100, 79), (78, 25, 14)]
-        '''
-        
-        red = []
-        green = []
-        blue = []
-        
-        for _ in range(self.n_colors):
-            while True:
-                r = np.random.randint(low = 0, high = 255, size = 1).tolist().pop()
-                if r not in red:
-                    red.append(r)
-                    break
+        """
+        if self.__rgb_0_255_list is None:
+            red = []
+            green = []
+            blue = []
             
-            while True:
-                g = np.random.randint(low = 0, high = 255, size = 1).tolist().pop()
-                if g not in green:
-                    green.append(g)
-                    break
+            for _ in range(self.n_colors):
+                r = self.random_int_generator()
+                while r in red:
+                    r = self.random_int_generator()
+                red.append(r)
+                
+                g = self.random_int_generator()
+                while g in green:
+                    g = self.random_int_generator()
+                green.append(g)
+                
+                b = self.random_int_generator()
+                while b in blue:
+                    b = self.random_int_generator()
+                blue.append(b)
             
-            while True:
-                b = np.random.randint(low = 0, high = 255, size = 1).tolist().pop()
-                if b not in blue:
-                    blue.append(b)
-                    break
-        
-        self.__rgb_0_255_list = list(zip(red, green, blue))
+            self.__rgb_0_255_list = list(zip(red, green, blue))
         
         return self.__rgb_0_255_list
-
+    
     def rgb_0_1(self) -> list:
-        '''
+        """
         Converts the values in __rgb_0_255_list from range 0 to 255 to 0.0 to 1.0
 
         Returns
@@ -89,15 +99,18 @@ class ColorCodesGenerator:
         Example:
             if __rgb_0_255_list = [(23, 100, 79), (78, 25, 14)], then the output will be:
             [(23/255, 100/255, 79/255), (78/255, 25/255, 14/255)] = [(0.09019, 0.39215, 0.30980), (0.30588, 0.09803, 0.05490)]
-        '''
-    
-        for r, g, b in self.__rgb_0_255_list:
-            self.__rgb_0_1_list.append((round(r/255, 5), round(g/255, 5), round(b/255, 5)))
+        """
+        if not self.__rgb_0_1_list:
+            if self.__rgb_0_255_list is None:
+                self.rgb_0_255()
+            
+            for r, g, b in self.__rgb_0_255_list:
+                self.__rgb_0_1_list.append((round(r / 255, 5), round(g / 255, 5), round(b / 255, 5)))
         
         return self.__rgb_0_1_list
-
+    
     def hex_codes(self) -> list:
-        '''
+        """
         Converts the values in __rgb_0_255_list from range 0 to 255 to hexa decimal system
         
         Returns
@@ -106,18 +119,22 @@ class ColorCodesGenerator:
         Example:
             if __rgb_0_255_list = [(23, 100, 79), (78, 25, 14)], then the output will be:
             ['#'17644F, '#4E190E']
-        '''
-        for r, g, b in self.__rgb_0_255_list:
-            hex_code = '#' + \
-                       self.suffix_0s(value = str(hex(r)[-2:])) + \
-                       self.suffix_0s(value = str(hex(g)[-2:])) + \
-                       self.suffix_0s(value = str(hex(b)[-2:]))
-            self.__hex_codes_list.append(hex_code.upper())
-
+        """
+        if not self.__hex_codes_list:
+            if self.__rgb_0_255_list is None:
+                self.rgb_0_255()
+            
+            for r, g, b in self.__rgb_0_255_list:
+                hex_code = '#' + \
+                           self.suffix_0s(value = str(hex(r)[2:])) + \
+                           self.suffix_0s(value = str(hex(g)[2:])) + \
+                           self.suffix_0s(value = str(hex(b)[2:]))
+                self.__hex_codes_list.append(hex_code.upper())
+        
         return self.__hex_codes_list
     
     def hsl_codes(self) -> list:
-        '''
+        """
         Converts the values in __rgb_0_255_list to HSL values (Hue, Saturation, Luminance)
         
         Returns
@@ -144,54 +161,95 @@ class ColorCodesGenerator:
             hue = 2.72728 * 60 = 163.6368 = 164 degrees
             
             So, hsl values = [(164, 63, 24)]
-        '''
+        """
+        if not self.__hsl_codes_list:
+            if not self.__rgb_0_1_list:
+                self.rgb_0_1()
+            
+            for r, g, b in self.__rgb_0_1_list:
+                # Calculating luminance
+                vmin = min(r, g, b)
+                vmax = max(r, g, b)
+                luminance = (vmin + vmax) / 2
+                
+                # Calculating saturation
+                if luminance <= 0.5:
+                    saturation = (vmax - vmin) / (vmax + vmin)
+                else:
+                    saturation = (vmax - vmin) / (2.0 - vmax - vmin)
+                
+                # Calculating hue
+                if r == vmax:
+                    hue = (g - b) / (vmax - vmin)
+                elif g == vmax:
+                    hue = 2.0 + ((b - r) / (vmax - vmin))
+                else:
+                    hue = 4.0 + ((r - g) / (vmax - vmin))
+                
+                if hue >= 0:
+                    hue *= 60
+                else:
+                    hue = (hue * 60) + 360
+                
+                h = int(round(hue, 0))
+                s = int(round(saturation * 100, 0))
+                l = int(round(luminance * 100, 0))
+                
+                self.__hsl_codes_list.append((h, s, l))
+        
+        return self.__hsl_codes_list
+    
+    def all_in_dataframe(self) -> pd.DataFrame:
+        """
+        This creates a dataframe of all color codes
+        """
+        if self.__rgb_0_255_list is None:
+            self.rgb_0_255()
         if not self.__rgb_0_1_list:
             self.rgb_0_1()
-
-        for r, g, b in self.__rgb_0_1_list:
-            # Calculating luminance
-            vmin = min(r, g, b)
-            vmax = max(r, g, b)
-            luminance = (vmin + vmax) / 2
-            
-            # Calculating saturation
-            if luminance <= 0.5:
-                saturation = (vmax - vmin)/(vmax + vmin)
-            else:
-                saturation = (vmax - vmin)/(2.0 - vmax - vmin)
-            
-            # Calculating hue
-            if r == vmax:
-                hue = (g - b)/(vmax - vmin)
-            elif g == vmax:
-                hue = 2.0 + ((b - r)/(vmax - vmin))
-            else:
-                hue = 4.0 + ((r - g)/(vmax - vmin))
-            
-            if hue >= 0:
-                hue *= 60
-            else:
-                hue = (hue * 60) + 360
-            
-            h = int(round(hue, 0))
-            s = int(round(saturation * 100, 0))
-            l = int(round(luminance * 100, 0))
-            
-            self.__hsl_codes_list.append((h, s, l))
-            
-        return self.__hsl_codes_list
+        if not self.__hex_codes_list:
+            self.hex_codes()
+        if not self.__hsl_codes_list:
+            self.hsl_codes()
+        return pd.DataFrame({'rgb_0_255': self.__rgb_0_255_list,
+                             'rgb_0_1'  : self.__rgb_0_1_list,
+                             'hex_codes': self.__hex_codes_list,
+                             'hsl_codes': self.__hsl_codes_list})
 
 
+def main():
+    ncols = int(input('Number of colors to generate: '))
+    print('''Available formats:
+        rgb_0_255   RGB values on a 0 to 255 scale (68, 125, 220)
+        rgb_0_1     RGB values on a 0.0 to 1.0 scale (0.257, 0.745, 0.947)
+        hex_code    Hexa decimal values (#B2E591)
+        hsl_code    Hue Saturation Luminance values (45, 97, 58)
+    ''')
+    code_format = input('Format you want (rgb_0_255, rgb_0,1, hex_code, hsl_code, all): ')
+    
+    col_gen = ColorCodesGenerator(n_colors = ncols)
+    if code_format == 'rgb_0_255':
+        print('====================== RGB (0 - 255) ======================')
+        print(col_gen.rgb_0_255())
+    elif code_format == 'rgb_0_1':
+        print('====================== RGB (0.0 - 1.0) ======================')
+        print(col_gen.rgb_0_1())
+    elif code_format == 'hex_code':
+        print('====================== HEX ======================')
+        print(col_gen.hex_codes())
+    elif code_format == 'hsl_code':
+        print('====================== HSL ======================')
+        print(col_gen.hsl_codes())
+    elif code_format == 'all':
+        print('====================== RGB (0 - 255) ======================')
+        print(col_gen.rgb_0_255())
+        print('====================== RGB (0.0 - 1.0) ======================')
+        print(col_gen.rgb_0_1())
+        print('====================== HEX ======================')
+        print(col_gen.hex_codes())
+        print('====================== HSL ======================')
+        print(col_gen.hsl_codes())
 
 
-
-x = ColorCodesGenerator(n_colors = 3)
-x.rgb_0_255()
-x.rgb_0_1()
-x.hex_codes()
-x.hsl_codes()
-
-y = ColorCodesGenerator(n_colors = 3)
-y.rgb_0_255()
-y.hex_codes()
-y.hsl_codes()
+if __name__ == '__main__':
+    main()
